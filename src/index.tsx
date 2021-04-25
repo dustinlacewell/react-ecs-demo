1d39ec90a0f7bd90f93ed39a46d3e758487ef241
Dustin Lacewell <dlacewell@gmail.com>
2021-04-24 20:05:36 -0500

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Entity, Facet, useAnimationFrame, useECS, useQuery, useSystem } from '@react-ecs/core'
+import { Vector3 } from 'three'
+import { ThreeView } from '@react-ecs/three'
+import { Torus } from '@react-three/drei'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Spin extends Facet<Spin> {
+  amount? = new Vector3(0, 1, 0)
+}
+
+const SpinSystem = () => {
+  const query = useQuery((e) => e.hasAll(ThreeView, Spin))
+
+  return useSystem((dt) => {
+    query.loop([ThreeView, Spin], (e, [view, spin]) => {
+      const rot = view.object3d.rotation
+      rot.x += spin.amount.x * dt
+      rot.y += spin.amount.y * dt
+      rot.z += spin.amount.z * dt
+    })
+  })
+}
+
+const CoolSim = () => {
+  const ECS = useECS()
+  useAnimationFrame(ECS.update)
+
+  return (
+    <Canvas>
+      <ECS.Provider>
+        <SpinSystem />
+        <Entity>
+          <Spin />
+          <ThreeView>
+            <Torus />
+          </ThreeView>
+        </Entity>
+      </ECS.Provider>
+    </Canvas>
+  )
+}
+
+ReactDOM.render(<CoolSim />, document.getElementById('root'))
